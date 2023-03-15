5614d9e82e74f1224ffdf0c854cc4f1225e26051
breeve <1205034601@qq.com>
2023-03-15 20:55:02 +0800

    2

--- dataplane/forward/main.c
+++ dataplane/forward/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char**argv) {
-    printf("hello world\n")
+#include "rte_eal.h"
+
+int main(int argc, char **argv) {
+    int i = 0;
+    for (; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+    rte_eal_init(argc, argv);
     return 0;
 }
--- mk/profile.mk
+++ mk/profile.mk
@@ -1,3 +1,5 @@
 ifndef NETWORK_PLATFORM_ROOT_PATH
 $(error "You need source profile")
 endif
+
+CC=gcc
